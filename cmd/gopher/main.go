525f3ab940e4f106ed69c879e7368ed723b4f425
takafk9 <narikawa.t@wano.co.jp>
2019-03-25 00:07:39 +0900

    add flag

--- cmd/gopher/main.go
+++ cmd/gopher/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"math/rand"
@@ -10,20 +11,32 @@ import (
 
 var out io.Writer = os.Stdout
 
+
 func main() {
+	flag.Parse()
+
+	const defaultPort = 3000
+	//受け取ったちをポインタとして返す
+	//port := flag.Int("port",defaultPort,"use")
+	//あらかじめ定義した変数にバインドする
+	var port int
+	flag.IntVar(&port,"port",defaultPort,"port to use")
+
 	fmt.Fprintln(out, "\x1b[2J")
 	gopherMsg := makeGopherMsg()
 	for i := 0; i < 75; i++ {
 		fmt.Fprintf(out, "\x1b[10;%dH\x1bK", i)
 		fmt.Print(gopherMsg)
 		time.Sleep(50 * time.Millisecond)
+
+
 	}
 }
 
 func makeGopherMsg() string {
 	rand.Seed(time.Now().UnixNano())
 	var gopherMsg string
-	randInt := rand.Intn(3)
+	randInt := rand.Intn(4)
 	switch randInt{
 	case 1:
 		gopherMsg = ` ʕ◔ϖ◔ʔ { Google万歳！！！！　`
@@ -31,6 +44,8 @@ func makeGopherMsg() string {
 		gopherMsg = ` ʕ◔ϖ◔ʔ { まだGOPATH以外で消耗してるの？ `
 	case 3:
 		gopherMsg = ` ʕ◔ϖ◔ʔ { まだGo言語以外で消耗してるの？ `
+	case 4:
+		gopherMsg = ` ʕ◔ϖ◔ʔ { お腹すいた `
 	default:
 		gopherMsg = ` ʕ◔ϖ◔ʔ { まだGo言語以外で消耗してるの？ `
 	}
